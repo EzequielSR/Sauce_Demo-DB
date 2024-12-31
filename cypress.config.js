08a2dc3a316a3217f7eeb9c555fca6cdd0d485ca
EzequielSR <ezequieldesr@gmail.com>
2024-12-31 00:07:31 -0300

    Update: Atualização do GitLab e da geração de relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -6,12 +6,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-  },
+
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     reportDir: 'cypress/reports',
     overwrite: false,
     html: true,
     json: true,
+    chart: true,
+    reportFilename: 'mochawesome-report'
   }
+  },
 });
